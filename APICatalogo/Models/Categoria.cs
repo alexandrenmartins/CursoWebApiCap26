3ffd9d8cf2064f4946556dd8a64e4513901efdc5
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2024-06-25 13:24:41 -0300

    Atualizacao tabelas

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categorias")] //Redundância pois o EF já atribui através do 'AppDbContext' que definimos.
 public class Categoria
 {
     public Categoria()
@@ -9,8 +12,15 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key] //Redundância pois o EF já atribui através da nomeclatura com o nome da entidade seguido de 'Id'.
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     public ICollection<Produto>? Produtos { get; set; }
