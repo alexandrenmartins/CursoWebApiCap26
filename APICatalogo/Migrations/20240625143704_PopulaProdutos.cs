8f5292257b8810fd6cacbbfebb0ecebca140d24e
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2024-07-09 16:33:29 -0300

    Teste

---
+++ APICatalogo/Migrations/20240625143704_PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "VALUES('Coca-Cola','Refri de cola',5.45,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "VALUES('Lanche de Atum','Lanche de Atum com molho',8.50,'atum.jpg',10,now(),2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "VALUES('Pudim','Pudim 100g',6.45,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Produtos");
+        }
+    }
+}
